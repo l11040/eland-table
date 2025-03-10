f3e6d7eca5dcf6ae77df303b4afc0003efb2c22f
l11040 <l1104033837@gmail.com>
2025-03-10 12:42:19 +0900

    [Package] 스토리북 초기 설정

---
+++ .storybook/main.ts
@@ -0,0 +1,21 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+import svgr from "vite-plugin-svgr";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  viteFinal: async (config) => {
+    config.plugins?.push(svgr());
+    return config;
+  },
+};
+export default config;
